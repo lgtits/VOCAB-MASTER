aa5d9dd8995edc031e124a94c48aa33dfab5ac52
a0937370704@gmail.com <a0937370704@gmail.com>
2026-04-30 17:38:22 +0800

    修改: fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // 假設你的專案名稱是 my-vocab-app
-  basePath: process.env.NODE_ENV === "production" ? "/my-vocab-app" : "",
-  images: { unoptimized: true },
+  // 必須與網址路徑完全一致
+  basePath: process.env.NODE_ENV === "production" ? "/VOCAB-MASTER" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/VOCAB-MASTER/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
